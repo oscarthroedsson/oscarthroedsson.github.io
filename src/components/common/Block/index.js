98d557de709fe7adee57b6aa06fdea33147daa05
Madeleine <madeleine.schonemann@gmail.com>
2020-11-18 22:05:05 +0100

    Updates

--- src/components/common/Block/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Block = ({ className, children }) => {
-  return (
-    <div className={`block ${className ? className : ""}`}>{children}</div>
-  );
-};
-
-export default Block;
--- src/components/common/Box/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./box.scss";
-
-const Box = ({ className, children }) => {
-  return <div className={`box ${className ? className : ""}`}>{children}</div>;
-};
-export default Box;
