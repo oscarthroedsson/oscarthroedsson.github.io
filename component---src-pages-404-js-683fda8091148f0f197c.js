f410a7a783f6d85051f130b27f369cc3ebab8c3b
Madeleine <madeleine.schonemann@gmail.com>
2020-11-24 21:11:12 +0100

    Updates

--- component---src-pages-404-js-683fda8091148f0f197c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),o=n.n(a),u=n("tioU"),l=n("WCSC");e.default=function(){return o.a.createElement(u.a,null,o.a.createElement(l.a,{title:"404: Not found"}),o.a.createElement("h1",null,"404: Not Found"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-683fda8091148f0f197c.js.map
