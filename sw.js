02f53fc74821440ec24ae82471992420f3421bf0
Madeleine <madeleine.schonemann@gmail.com>
2020-12-03 21:51:32 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-119cafa82a2758a1e714.js"
+    "url": "webpack-runtime-dd6467ec5dbf94ff0622.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2d74648c5fde728e8ebf8660208a392d"
+    "revision": "865a7aa7ef0a648456ccbb3a3257e08c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
