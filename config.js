4ab48c808402b6cb9e0459cf9a156e0543582bc1
Madeleine <madeleine.schonemann@gmail.com>
2020-12-06 20:48:54 +0100

    Update metadata

--- config.js
+++ config.js
@@ -1,35 +1,35 @@
 module.exports = {
-  siteTitle: 'Oscar Throedsson - Personlig Tränare & Coach', // <title>
-  manifestName: 'Oscar Throedsson',
-  manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#00587c',
-  manifestThemeColor: '#00587c',
-  manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: '', // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Oscar Throedsson',
-  subHeading: 'När hälsa blir en investering',
+  siteTitle: "Oscar Throedsson - Din personliga tränare", // <title>
+  manifestName: "Oscar Throedsson",
+  manifestShortName: "Oscar Throedsson", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: "#00587c",
+  manifestThemeColor: "#00587c",
+  manifestDisplay: "standalone",
+  manifestIcon: "src/assets/img/website-icon.png",
+  pathPrefix: "",
+  heading: "Oscar Throedsson",
+  subHeading: "Din personliga tränare",
 
   // social
   socialLinks: [
     {
-      icon: 'fa-linkedin-in',
-      name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/oscar-throedsson-8586ba124/',
+      icon: "fa-linkedin-in",
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/oscar-throedsson-8586ba124/",
     },
     {
-      icon: 'fa-instagram',
-      name: 'Instagram',
-      url: 'https://instagram.com/oscarthroedsson',
+      icon: "fa-instagram",
+      name: "Instagram",
+      url: "https://instagram.com/oscarthroedsson",
     },
     {
-      icon: 'fa-facebook-f',
-      name: 'Facebook',
-      url: 'https://www.facebook.com/OscarThroedsson-108575847730516',
+      icon: "fa-facebook-f",
+      name: "Facebook",
+      url: "https://www.facebook.com/OscarThroedsson-108575847730516",
     },
   ],
-  email: 'oscar.throedsson@gmail.com',
-  phone: '000-00000',
-  address: '305 Del Sol, Sunnyvale, CA',
+  email: "oscar.throedsson@gmail.com",
+  phone: "000-00000",
+  address: "305 Del Sol, Sunnyvale, CA",
 };
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -26,15 +26,17 @@ class Layout extends Component {
               meta={[
                 {
                   name: "description",
-                  content: "När hälsa blir en investering.",
+                  content:
+                    "Med glimten i ögat och ett driv för resultat så ser jag till att din träning går framåt men också är rolig på samma gång. Med över 10 000 timmar på golvet med kunder så har jag fått chansen att utveckla träning och kostupplägg för att det ska passa kundens behov och förutsättningar. Tillsammans hittar vi vägen för att du ska lyckas.",
                 },
                 {
                   name: "keywords",
-                  content: "personlig tränare, pt, coach, träning, hälsa",
+                  content:
+                    "Personlig träning, Personlig tränare, Personlig Tränare lund, onlinecoach, PT online, träningsprogram, kostprogram, kostupplägg, viktnedgång, Uteträning, uteträning lund, företagshälsa, friskvård",
                 },
               ]}
             >
-              <html lang="en" />
+              <html lang="sv" />
             </Helmet>
             <div className={"page-top"}>{children}</div>
           </>
